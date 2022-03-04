161646bafc1bc6ac46c87ab1e63ca24de028804e
thecef02 <thecef@hotmail.com>
2022-03-03 18:19:14 -0700

    From Cristian

---
+++ snake/__main__.py
@@ -0,0 +1,47 @@
+import constants
+
+from game.casting.cast import Cast
+from game.casting.food import Food
+from game.casting.score import Score
+from game.casting.snake import Snake
+from game.scripting.script import Script
+from game.scripting.control_actors_action import ControlActorsAction
+from game.scripting.move_actors_action import MoveActorsAction
+from game.scripting.handle_collisions_action import HandleCollisionsAction
+from game.scripting.draw_actors_action import DrawActorsAction
+from game.directing.director import Director
+from game.services.keyboard_service import KeyboardService
+from game.services.video_service import VideoService
+from game.shared.color import Color
+from game.shared.point import Point
+
+
+def main():
+    
+    # create the cast
+    cast = Cast()
+    cast.add_actor("foods", Food())
+    s1 = Snake()
+    s1.prepare_body("s1")
+    cast.add_actor("snakes", s1)
+    s2 = Snake()
+    s2.prepare_body("")
+    cast.add_actor("snakes2", s2)
+    cast.add_actor("scores", Score())
+   
+    # start the game
+    keyboard_service = KeyboardService()
+    video_service = VideoService()
+
+    script = Script()
+    script.add_action("input", ControlActorsAction(keyboard_service))
+    script.add_action("update", MoveActorsAction())
+    script.add_action("update", HandleCollisionsAction())
+    script.add_action("output", DrawActorsAction(video_service))
+    
+    director = Director(video_service)
+    director.start_game(cast, script)
+
+
+if __name__ == "__main__":
+    main()
